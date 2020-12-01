a68a42a338ded32969de672cdd802d4993c9d8d8
Ankie Bosman <ankie555@hotmail.com>
2020-12-01 09:08:19 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllLines(@"Input.txt");
+var expenses = input.Select(c => int.Parse(c.ToString())).ToList();
+
+PartA();
+
+void PartA()
+{
+    foreach (var (expense, expense2) in from expense in expenses
+        let expense2 = expenses.Find(e => expense + e == 2020)
+        where expense2 != 0
+        select (expense, expense2))
+    {
+        Console.WriteLine($"{expense} + {expense2} = 2020");
+        Console.WriteLine($"result = {expense} * {expense2} = {expense * expense2}");
+        return;
+    }
+}
