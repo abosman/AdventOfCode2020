0f2f3463b025586d7e5fc50709bc98724fbf4ebd
Ankie Bosman <ankie555@hotmail.com>
2020-12-01 09:21:45 +0100

    Part2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -5,9 +5,10 @@ using System.Linq;
 var input = File.ReadAllLines(@"Input.txt");
 var expenses = input.Select(c => int.Parse(c.ToString())).ToList();
 
-PartA();
+Part1();
+Part2();
 
-void PartA()
+void Part1()
 {
     foreach (var (expense, expense2) in from expense in expenses
         let expense2 = expenses.Find(e => expense + e == 2020)
@@ -17,5 +18,24 @@ void PartA()
         Console.WriteLine($"{expense} + {expense2} = 2020");
         Console.WriteLine($"result = {expense} * {expense2} = {expense * expense2}");
         return;
+    }
+}
+
+void Part2()
+{
+    for (int i = 0; i < expenses.Count-2; i++)
+    {
+        for (int j = 1; j < expenses.Count-1; j++)
+        {
+            for (int k = 2; k < expenses.Count; k++)
+            {
+                if (expenses[i] + expenses[j] + expenses[k] == 2020)
+                {
+                    Console.WriteLine($"{expenses[i]} + {expenses[j]} + {expenses[k]} = 2020");
+                    Console.WriteLine($"result = {expenses[i]} * {expenses[j]} * {expenses[k]} = {expenses[i] * expenses[j] * expenses[k]}");
+                    return;
+                }
+            }
+        }
     }
 }
