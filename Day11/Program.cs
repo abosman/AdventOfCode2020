8924a61b73935b68c03f922cb7767dfa96830973
Ankie Bosman <ankie555@hotmail.com>
2020-12-12 22:11:27 +0100

    Day 11 + Day 12

---
+++ Day11/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+var sw = new Stopwatch();
+sw.Start();
+var inputFileName = "Input.txt";
+var map = ReadInput(inputFileName);
+Part1();
+map = ReadInput(inputFileName);
+Part2();
+Console.WriteLine($"Total time: {sw.Elapsed.TotalMilliseconds}");
+
+void Part1()
+{
+    var change = true;
+    while (change)
+    {
+        change = false;
+        var prevMap = map.Clone() as char[,];
+        for (var i = 0; i <= map.GetUpperBound(0); i++)
+        {
+            for (var j = 0; j <= map.GetUpperBound(1); j++)
+            {
+                var point = prevMap[i, j];
+                if (point == 'L' && OccupiedAdjacentSeats(prevMap, i, j) == 0)
+                {
+                    map[i, j] = '#';
+                    change = true;
+                }
+                else if (point == '#' && OccupiedAdjacentSeats(prevMap, i, j) >= 4)
+                {
+                    map[i, j] = 'L';
+                    change = true;
+                }
+            }
+        }
+    }
+    Console.WriteLine($"Number of occupied seats: {NumberOfOccupiedSeats(map)}"); // 2489
+}
+void Part2()
+{
+    var change = true;
+    while (change)
+    {
+        change = false;
+        var prevMap = map.Clone() as char[,];
+        for (int i = 0; i <= map.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= map.GetUpperBound(1); j++)
+            {
+                var point = prevMap[i, j];
+                if (point == 'L' && OccupiedSeats(prevMap, i, j) == 0)
+                {
+                    map[i, j] = '#';
+                    change = true;
+
+                }
+                else if (point == '#' && OccupiedSeats(prevMap, i, j) >= 5)
+                {
+                    map[i, j] = 'L';
+                    change = true;
+                }
+            }
+        }
+        //PrintGrid(map);
+    }
+    Console.WriteLine($"Number of occupied seats: {NumberOfOccupiedSeats(map)}"); // 2180
+}
+
+int OccupiedAdjacentSeats(char[,] map, int row, int col)
+{
+    var occupiedSeats = 0;
+    var startColumn = Math.Max(col - 1, 0);
+    var endColumn = Math.Min(col + 1, map.GetUpperBound(1));
+    var startRow = Math.Max(row - 1, 0);
+    var endRow = Math.Min(row + 1, map.GetUpperBound(0));
+
+    for (int i = startRow; i <= endRow; i++)
+    {
+        for (int j = startColumn; j <= endColumn; j++)
+        {
+            if (i == row && j == col)
+            {
+                continue;
+            }
+            if (map[i, j] == '#')
+            {
+                occupiedSeats++;
+            }
+        }
+    }
+    return occupiedSeats;
+}
+
+int OccupiedSeats(char[,] map, int row, int col)
+{
+    var occupiedSeats = 0;
+    for (int i = -1; i < 2; i++)
+    {
+        for (int j = -1; j < 2; j++)
+        {
+            if (i == 0 && j == 0)
+            {
+                continue;
+            }
+            if (FindOccupiedSeat(map,row, col, i, j))
+            {
+                occupiedSeats++;
+            }
+        }
+    }
+    return occupiedSeats;
+}
+
+bool FindOccupiedSeat(char[,] map, int currentRow, int currentCol, int rowOffset, int colOffset)
+{
+
+    var row = currentRow + rowOffset;
+    var endRow = map.GetUpperBound(0);
+    var col = currentCol + colOffset;
+    var endColumn = map.GetUpperBound(1);
+
+    while (true)
+    {
+        if (row < 0 || row > endRow || col < 0 || col > endColumn)
+        {
+            return false;
+        }
+        switch (map[row, col])
+        {
+            case '#':
+                return true;
+            case 'L':
+                return false;
+        }
+        col += colOffset;
+        row += rowOffset;
+    }
+}
+
+int NumberOfOccupiedSeats(char[,] map)
+{
+    var numberOfOccupiedSeats = 0;
+    for (var i = 0; i <= map.GetUpperBound(0); i++)
+    {
+        for (var j = 0; j <= map.GetUpperBound(1); j++)
+        {
+            if (map[i, j] == '#')
+            {
+                numberOfOccupiedSeats++;
+            }
+        }
+    }
+    return numberOfOccupiedSeats;
+}
+
+static void PrintGrid(char[,] map)
+{
+    for (int i = 0; i <= map.GetUpperBound(0); i++)
+    {
+        for (int j = 0; j <= map.GetUpperBound(1); j++)
+        {
+            Console.Write(map[i, j]);
+        }
+
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+
+char[,] ReadInput(string fileName)
+{
+    var lines = File.ReadAllLines(fileName);
+    var map = new char[lines.Length, lines[0].Length];
+    for (int i = 0; i < lines.Length; i++)
+    {
+        var line = lines[i];
+        for (int j = 0; j < line.Length; j++)
+        {
+            map[i, j] = line[j];
+        }
+    }
+    return map;
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,120 @@
+﻿using Day12;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+var sw = new Stopwatch();
+sw.Start();
+var inputFileName = "Input.txt";
+var lines = File.ReadAllLines(inputFileName);
+var instructions = lines.Select(line =>
+    new Instruction(line[0].ToString(), int.Parse(line[1..]))).ToList();
+
+Part1();
+Part2();
+Console.WriteLine($"Total time: {sw.Elapsed.TotalMilliseconds}");
+
+void Part1()
+{
+    var position = new Tuple<int, int, Direction>(0, 0, Direction.East);
+    position = instructions.Aggregate(position, (current, instruction) => instruction.Action switch
+    {
+        "N" => new Tuple<int, int, Direction>(current.Item1, current.Item2 + instruction.Step, current.Item3),
+        "S" => new Tuple<int, int, Direction>(current.Item1, current.Item2 - instruction.Step, current.Item3),
+        "W" => new Tuple<int, int, Direction>(current.Item1 - instruction.Step, current.Item2, current.Item3),
+        "E" => new Tuple<int, int, Direction>(current.Item1 + instruction.Step, current.Item2, current.Item3),
+        "F" => MoveForward(current, instruction.Step),
+        "L" => new Tuple<int, int, Direction>(current.Item1, current.Item2, DetermineDirection(current.Item3, -instruction.Step)),
+        "R" => new Tuple<int, int, Direction>(current.Item1, current.Item2, DetermineDirection(current.Item3, instruction.Step)),
+        _ => throw new ArgumentOutOfRangeException()
+    });
+    var distance = Math.Abs(position.Item1) + Math.Abs(position.Item2);
+    Console.WriteLine($"Manhattan distance = {distance}"); // 2879
+}
+
+Tuple<int, int, Direction> MoveForward(Tuple<int, int, Direction> currentPosition, int step)
+{
+    return currentPosition.Item3 switch
+    {
+        Direction.North => new Tuple<int, int, Direction>(currentPosition.Item1, currentPosition.Item2 + step,
+            currentPosition.Item3),
+        Direction.South => new Tuple<int, int, Direction>(currentPosition.Item1, currentPosition.Item2 - step,
+            currentPosition.Item3),
+        Direction.West => new Tuple<int, int, Direction>(currentPosition.Item1 - step, currentPosition.Item2,
+            currentPosition.Item3),
+        Direction.East => new Tuple<int, int, Direction>(currentPosition.Item1 + step, currentPosition.Item2,
+            currentPosition.Item3),
+        _ => throw new ArgumentOutOfRangeException()
+    };
+}
+
+
+Direction DetermineDirection(Direction current, int degrees)
+{
+    return (Direction)(((int)current + (degrees / 90) + 4) % 4);
+}
+
+void Part2()
+{
+    var ship = new Tuple<int, int>(0, 0);
+    var wayPoint = new Tuple<int, int>(10, 1);
+    foreach (var instruction in instructions)
+    {
+        switch (instruction.Action)
+        {
+            case "F":
+                ship = new Tuple<int, int>(ship.Item1 + wayPoint.Item1 * instruction.Step,
+                    ship.Item2 + wayPoint.Item2 * instruction.Step);
+                break;
+            case "N":
+                wayPoint =
+                    new Tuple<int, int>(wayPoint.Item1, wayPoint.Item2 + instruction.Step);
+                break;
+            case "S":
+                wayPoint =
+                    new Tuple<int, int>(wayPoint.Item1, wayPoint.Item2 - instruction.Step);
+                break;
+            case "W":
+                wayPoint =
+                    new Tuple<int, int>(wayPoint.Item1 - instruction.Step, wayPoint.Item2);
+                break;
+            case "E":
+                wayPoint =
+                    new Tuple<int, int>(wayPoint.Item1 + instruction.Step, wayPoint.Item2);
+                break;
+            case "R":
+                wayPoint = RotateWayPoint(wayPoint, instruction.Step);
+                break;
+            case "L":
+                wayPoint = RotateWayPoint(wayPoint, -instruction.Step);
+                break;
+        }
+    }
+    var distance = Math.Abs(ship.Item1) + Math.Abs(ship.Item2);
+    Console.WriteLine($"Manhattan distance = {distance}"); // 178986
+}
+
+Tuple<int, int> RotateWayPoint(Tuple<int, int> positionWayPoint, int degrees)
+{
+    var step = (degrees / 90 + 4) % 4;
+    var point = positionWayPoint;
+    for (var i = 0; i < step; i++)
+    {
+        point = new Tuple<int, int>(point.Item2, point.Item1 * -1);
+    }
+    return point;
+}
+
+namespace Day12
+{
+    record Instruction(string Action, int Step);
+
+    enum Direction
+    {
+        East = 0,
+        South = 1,
+        West = 2,
+        North = 3
+    }
+}
